7762904978212874e0862f3f174849918bdf168e
Heather Mills <heathermills1980@gmail.com>
2022-10-10 08:14:51 -0500

    Completed Exercise

---
+++ script.js
@@ -0,0 +1,52 @@
+// 1a
+let warmHugs = "Hi \, I\'m Olaf and I like warm hugs.";
+
+// 1b
+console.log(warmHugs.toUpperCase());
+
+// 1c
+warmHugs = warmHugs.replace("like", "love");
+console.log(warmHugs);
+
+// 2a
+let beenImpaled = "Oh, look at that. I've been impaled.";
+
+// 2b
+console.log(beenImpaled.slice(18,36));
+
+// 3a
+let dotDotDot = "...";
+
+// 3b
+let skullBones = 'I don't have a skull${dotDotDOt} or bones';
+
+// 4
+console.log(Math.PI);
+
+// 5
+let randomNumber = Math.random(); // 0 through 0.999999
+randomNumber *=3; // 0 to 2.999999
+randomNumber = Math.floor(randomNumber); // gets rid of decimal (0 to 2)
+randomNumber ++;
+
+// BONUS
+// 6
+let go = "Let It Go!";
+console.log("Let It Go!".repeat(2).toUpperCase());
+
+// 7a
+let reindeers = "Reindeers are better than people.";
+
+// 7b
+console.log(reindeers.replace("Reindeers are better than people.", "Reindeers_are
+_better_than_people"));
+
+// 8
+console.log(Math.sqrt(2));
+
+// 9
+let newRandomNumber = math.floor((Math.random()*(17 + 7));
+
+
+
+
